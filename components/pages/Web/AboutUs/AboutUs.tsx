78c616b557f52edcc61e5f0bd9276f922d2faf09
lakshyabatman <lakshya.khera@gmail.com>
2022-08-02 15:42:36 +0530

    fixes

--- components/pages/Web/AboutUs/AboutUs.tsx
+++ components/pages/Web/AboutUs/AboutUs.tsx
@@ -10,7 +10,7 @@ const AboutUs = () => {
         <div className='flex justify-start'>
             <img src={BuidlDAOHeroImg.src} />
         </div>
-        <div className='flex justify-center flex-col'>
+        <div className='flex justify-center flex-col w-9/12	'>
             <h1 className='text-4xl font-bold'>Introducing <span className='text-blue-500'>Buidl Token</span>, which powers BuidlDAO</h1>
             <p className='mt-3  opacity-40 text-lg'>Buidl Token is ERC-20 token on Polygon, used buy BuidlDAO to operate over chain.</p>
           
