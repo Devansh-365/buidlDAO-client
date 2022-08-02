ea43e24b41eff1ab7cbaa780e9677eaba03863e6
lakshyabatman <lakshya.khera@gmail.com>
2022-08-02 15:55:56 +0530

    fixes

--- components/pages/Web/Footer/Footer.tsx
+++ components/pages/Web/Footer/Footer.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Logo from '../../../../assets/Logo.png'
 const Footer = () => {
   return (
-    <div className='bg-black p-10 flex justify-between text-white'>
-        <div className='container mx-auto px-8'>
+    <div className='bg-black p-5  text-white'>
+        <div className='container mx-auto px-8 flex justify-between items-center'>
             <img src={Logo.src}/>
-        </div>
-        <div >
+            <div >
             <span className='mr-6'>Whitepaper</span>
             <span>Discord</span>
+            </div>
         </div>
+       
     </div>
   )
 }
